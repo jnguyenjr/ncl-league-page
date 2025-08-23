fdcf111a42a3f3ef253232c61858509d9dbcf225
jnguyenjr <jnguyenjr@gmail.com>
2025-08-23 10:06:36 -0700

    Update leagueInfo.js
    Fixing typo on vans manager

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -168,7 +168,7 @@ export const managers = [
         link: 9, // manager array number within this array, or null to link back to all managers page
         image: "/managers/sang_h.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-      "favoritePlayer": nill, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "I don’t think I’ll ever win",
